f2b13ad8b7f03dd3d377cff62e5a0ffe97965200
potchakik <santhonydan7@yahoo.com>
2022-03-08 06:08:32 +0800

    ADD search input

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 var axios = require("axios").default;
 
@@ -7,43 +7,45 @@ const cryptoApiHeaders = {
   "x-rapidapi-key": "ef72cb1fddmsh2af6bb98e2409a9p18bec2jsn0d0023bdb065",
 };
 
-const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
-const params = {
-  referenceCurrencyUuid: "yhjMzLPhuIDl",
-  timePeriod: "24h",
-  tiers: "1",
-  orderBy: "marketCap",
-  orderDirection: "desc",
-  limit: "50",
-  offset: "0",
-};
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+//const params = {
+//  referenceCurrencyUuid: "yhjMzLPhuIDl",
+//  timePeriod: "24h",
+//  tiers: "1",
+//  orderBy: "marketCap",
+//  orderDirection: "desc",
+//  limit: "50",
+//  offset: "0",
+//};
 
-var options = {
-  method: "GET",
-  url: "https://coinranking1.p.rapidapi.com/coins",
-  params: {
-    referenceCurrencyUuid: "yhjMzLPhuIDl",
-    timePeriod: "24h",
-    tiers: "1",
-    orderBy: "marketCap",
-    orderDirection: "desc",
-    limit: "50",
-    offset: "0",
-  },
-  headers: {
-    "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-    "x-rapidapi-key": "ef72cb1fddmsh2af6bb98e2409a9p18bec2jsn0d0023bdb065",
-  },
-};
+//var options = {
+//  method: "GET",
+//  url: "https://coinranking1.p.rapidapi.com",
+//  params: {
+//    referenceCurrencyUuid: "yhjMzLPhuIDl",
+//    timePeriod: "24h",
+//    tiers: "1",
+//    orderBy: "marketCap",
+//    orderDirection: "desc",
+//    limit: "50",
+//    offset: "0",
+//  },
+//  headers: {
+//    "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//    "x-rapidapi-key": "ef72cb1fddmsh2af6bb98e2409a9p18bec2jsn0d0023bdb065",
+//  },
+//};
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-export const cyptoApi = createApi({
+export const cryptoApi = createApi({
   reducerPath: "cryptopApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest("/exchages"),
+      query: (count) => createRequest(`/coins?limit=${count}`),
     }),
   }),
 });
+
+export const { useGetCryptosQuery } = cryptoApi;
