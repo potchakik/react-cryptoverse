904c0827c7ff22c9e0a9ab8c5a991e65f6faeb3f
potchakik <santhonydan7@yahoo.com>
2022-03-08 04:23:07 +0800

    add coins api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,5 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
 var axios = require("axios").default;
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "ef72cb1fddmsh2af6bb98e2409a9p18bec2jsn0d0023bdb065",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+const params = {
+  referenceCurrencyUuid: "yhjMzLPhuIDl",
+  timePeriod: "24h",
+  tiers: "1",
+  orderBy: "marketCap",
+  orderDirection: "desc",
+  limit: "50",
+  offset: "0",
+};
 
 var options = {
   method: "GET",
@@ -18,3 +35,15 @@ var options = {
     "x-rapidapi-key": "ef72cb1fddmsh2af6bb98e2409a9p18bec2jsn0d0023bdb065",
   },
 };
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cyptoApi = createApi({
+  reducerPath: "cryptopApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchages"),
+    }),
+  }),
+});
