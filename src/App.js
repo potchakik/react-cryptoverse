28feeecb8d1184f53c6f3c417214df5b3a7402a4
potchakik <santhonydan7@yahoo.com>
2022-03-07 20:20:47 +0800

    added Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
-
-import { Navbar } from "./components";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  CryptoDetails,
+  Cryptocurrencies,
+  News,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import {
 } from "@ant-design/icons";
 
 import icon from "../images/cryptocurrency.png";
+import MenuItem from "antd/lib/menu/MenuItem";
 
 const Navbar = () => {
   return (
@@ -23,6 +24,20 @@ const Navbar = () => {
 
         </Button>*/}
       </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchages">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
